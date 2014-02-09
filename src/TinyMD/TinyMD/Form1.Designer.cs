7d292ce8e8fe22e94347cb1ad43c59006dfdacb4
balb <markwbmx@gmail.com>
2014-02-09 21:41:59 +0000

    Initial files

---
+++ src/TinyMD/TinyMD/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace TinyMD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Enabled = false;
+            this.btnSaveFile.Location = new System.Drawing.Point(93, 12);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveFile.TabIndex = 1;
+            this.btnSaveFile.Text = "Save File";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 41);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(456, 379);
+            this.splitContainer1.SplitterDistance = 220;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 373);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.AllowWebBrowserDrop = false;
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(4, 4);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(225, 372);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 432);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.btnSaveFile);
+            this.Controls.Add(this.btnOpenFile);
+            this.Name = "Form1";
+            this.Text = "TinyMD";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btnSaveFile;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ src/TinyMD/TinyMD/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TinyMD
+{
+    // TODO: Open with command line stuff
+    // TODO: Spellchecker
+    // TODO: Ctrl+S
+
+    public partial class Form1 : Form
+    {
+        string _tempFile;
+        string _currentFile;
+        MarkdownSharp.Markdown _markdown = new MarkdownSharp.Markdown();
+        public Form1()
+        {
+            InitializeComponent();
+            // We need to give the web browser a document
+            _tempFile = Path.GetTempFileName();
+            File.WriteAllText(_tempFile, " ");
+            webBrowser1.Navigate(_tempFile);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            webBrowser1.DocumentText = "<div style='font-family: Helvetica,arial,freesans,clean,sans-serif;'>" +
+                                        _markdown.Transform(textBox1.Text)
+                                        + "</div>";
+            btnSaveFile.Enabled = true;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            try
+            {
+                // Clean up
+                if (File.Exists(_tempFile))
+                    File.Delete(_tempFile);
+            }
+            catch { }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Markdown files (*.md;*.markdown)|*.md;*.markdown";
+            if (fileDialog.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+            {
+                var stream = fileDialog.OpenFile();
+                if (stream != null)
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
+                        textBox1.Text = reader.ReadToEnd();
+                    }
+                    stream.Close();
+                    _currentFile = fileDialog.FileName;
+                    this.Text = _currentFile;
+                }
+            }
+
+            btnSaveFile.Enabled = false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(_currentFile))
+            {
+                File.WriteAllText(_currentFile, textBox1.Text);
+            }
+            else
+            {
+                var fileDialog = new SaveFileDialog();
+                fileDialog.Filter = "Markdown files (*.md;*.markdown)|*.md;*.markdown";
+                if (fileDialog.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+                {
+                    var stream = fileDialog.OpenFile();
+                    if (stream != null)
+                    {
+                        using (var writer = new StreamWriter(stream))
+                        {
+                            writer.Write(textBox1.Text);
+                        }
+                        stream.Close();
+                        _currentFile = fileDialog.FileName;
+                        this.Text = _currentFile;
+                    }
+                }
+            }
+
+            btnSaveFile.Enabled = false;
+        }
+    }
+}
